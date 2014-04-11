a7c562e92f13eaa129290d820931905171b3f9ce
Remy Carron <carronr@pc-a104-10.iut-valence.fr>
2014-04-11 12:01:44 +0200

    Ajout formulaire authentification et début formulaire inscription

--- promotion/views.py
+++ promotion/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm
+from promotion.forms import UserCreateForm
 
 # Create your views here.
 def index(request):
-    
-    context = {
-               "message": "Page d'accueil",
-               }
-    
-    return render(request, "promotion/index.html", context)
+    if request.method == 'POST': # If the form has been submitted...
+        # ContactForm was defined in the the previous section
+        print request.POST
+        form = AuthenticationForm(data=request.POST) # A form bound to the POST data
+        
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            # ...
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = AuthenticationForm() # An unbound form
+
+    return render(request, 'promotion/index.html', {
+        'form_auth': form,
+    })
