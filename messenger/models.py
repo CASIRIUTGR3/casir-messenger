c070144b86b879df54b3d522f6ad6c1c78b3a7ad
Aurelie Gerland <gerlanda@pc-a102-09.iut-valence.fr>
2014-04-17 14:43:56 +0200

    messenger db et model

--- messenger/models.py
+++ messenger/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Messenger(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    messenger = models.ForeignKey(Messenger)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
